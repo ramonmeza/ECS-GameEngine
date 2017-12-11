d140f274fe51d30a6039b47ae419c654b47e645e
Ramon Meza <ramon_meza@live.com>
2017-12-11 09:45:22 -0500

    Tested correctness of SpriteComponent::Tick(float) and TransformComponent

--- ECS-GameEngine/main.cpp
+++ ECS-GameEngine/main.cpp
@@ -14,6 +14,9 @@ int main(int argc, char* argv[])
 	Entity ent;
 	ent.AddComponent(new SpriteComponent("assets/sprites/sprite.png", &ent));
 
+	sf::Clock deltaClock;
+	sf::Time deltaTime = deltaClock.restart();
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -23,9 +26,14 @@ int main(int argc, char* argv[])
 				window.close();
 		}
 
+		ent.Tick(deltaTime.asSeconds());
+		ent.transform->position += sf::Vector2f(10.0f, 10.0f) * deltaTime.asSeconds();
+
 		window.clear();
 		window.draw(ent);
 		window.display();
+
+		deltaTime = deltaClock.restart();
 	}
 
 	return 0;
